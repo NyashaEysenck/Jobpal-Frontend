db72ee0e9ad2277faae6e2991011ae0dff65dadd
Eysenck Gandah <nyashafpl@gmail.com>
2025-03-27 18:52:48 +0200

    update

--- src/pages/InterviewQuestions.tsx
+++ src/pages/InterviewQuestions.tsx
@@ -23,7 +23,6 @@ const InterviewQuestions: React.FC = () => {
     try {
       const response = await fetch(`${import.meta.env.VITE_APP_BASE_URL}/interview-questions`, {
         method: 'POST',
-        credentials: 'include',
         headers: {
           'Content-Type': 'application/json',
         },
